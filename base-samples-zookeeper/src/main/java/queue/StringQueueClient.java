090caf0e9fb81f4be7963f46ca5a9961b2703cc9
cow-boy <hellohuxu@qq.com>
2017-07-26 16:01:38 +0800

    zookeeper

---
+++ base-samples-zookeeper/src/main/java/queue/StringQueueClient.java
@@ -0,0 +1,51 @@
+package queue;
+
+import org.apache.curator.framework.CuratorFramework;
+import org.apache.curator.framework.recipes.queue.DistributedQueue;
+import org.apache.curator.framework.recipes.queue.QueueBuilder;
+import org.apache.curator.framework.recipes.queue.QueueConsumer;
+import utils.ClientFactory;
+
+public class StringQueueClient {
+
+	private static final String PATH = "/queuetest";
+	private static final String LOCK_PATH = "/queuelock";
+	private DistributedQueue<String> queue;
+
+	private static CuratorFramework client = ClientFactory.newClient();
+
+	public StringQueueClient(int length) {
+		try {
+			client.start();
+			QueueBuilder<String> builder = QueueBuilder.builder(client, null, new StringQueueSerializer(), PATH);
+			builder.maxItems(length);
+			queue = builder.lockPath(LOCK_PATH).buildQueue();// 消费担保
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public StringQueueClient(QueueConsumer<String> handler) {
+		try {
+			CuratorFramework client = ClientFactory.newClient();
+			client.start();
+			QueueBuilder<String> builder = QueueBuilder.builder(client, handler, new StringQueueSerializer(), PATH);
+			queue = builder.lockPath(LOCK_PATH).buildQueue();// 消费担保
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void put(String message) throws Exception {
+		queue.put(message);
+	}
+
+	public void start() throws Exception {
+		queue.start();
+	}
+
+	public void close() throws Exception {
+		queue.close();
+	}
+
+}
